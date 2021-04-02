e393e42cea4738314719d1182da98c37e2764387
vinayv_456 <vinayvallabineni456@gmail.com>
2021-04-02 17:24:06 +0530

    Add axios for accessing api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import {useState, useEffect} from 'react';
 import './App.css';
+
+//import components 
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+
+//imort client api
+import axiosInstance from './apis/client'
+
 import React from 'react';
 
 function App() {
   const [tempInput, setTempInput] = useState('')
   const [imageUrl, setImageUrl] = useState('')
-  useEffect(()=>{
 
+  useEffect(()=>{
+    axiosInstance.get('/users')
+    .then(result => console.log(result.data))
+    .catch(e=>console.log(e))
   }, [])
 
   const onInputChange = (e) => {
