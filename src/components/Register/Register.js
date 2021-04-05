d85072fdc57fed787fedae2f3ac4ef205573a996
vinayv-456 <vinayvallabineni456@gmail.com>
2021-04-05 23:01:50 +0530

    Add register route

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,36 +2,41 @@ import React, { useState, useEffect } from 'react';
 
 import axiosInstance from '../../apis/client'
 
-const Register = () => {
+const Register = ({ handleRoute }) => {
   const [form, setForm] = useState({}) 
 
   const onNameChange = (event) => {
-    setForm(...form, {name: event.target.value})
+    let name = event.target.value
+    setForm({...form, name: name})
   }
 
   const onEmailChange = (event) => {
-    setForm(...form, {email: event.target.value})
+    setForm({...form, email: event.target.value})
   }
 
   const onPasswordChange = (event) => {
-    setForm(...form, {password: event.target.value})
+    setForm({...form, password: event.target.value})
   }
 
-  const onSubmitSignIn = async() => {
+  const handleSignUp = async() => {
     console.log(form)
     const result = await axiosInstance.post('/sign-up', {...form})
+    if(result)
+    handleRoute("home");
   }
 
     return (
-      <div>
-        Name:
-        <input type="text" onInput={onNameChange}></input>
-        Email:
-        <input type="text" onInput={onEmailChange}/>
-        password:
-        <input type="text" onInput={onPasswordChange}/>
+      <div style={{display: 'flex', justifyContent:'center'}}>
+        <div style={{display:'flex', flexDirection:'column', alignContent:'center', width:'50%'}}>
+          Name:
+          <input type="text" onInput={onNameChange} style={{marginBottom:'10px'}}></input>
+          Email:
+          <input type="text" onInput={onEmailChange} style={{marginBottom:'10px'}}/>
+          password:
+          <input type="text" onInput={onPasswordChange} style={{marginBottom: '20px'}}/>
 
-        <button onClick={onSubmitSignIn()}>Register</button>
+          <button onClick={handleSignUp}>Register</button>
+        </div>
       </div>
     );
   }
