1c6c033427479d1cec8919cca3cc18f9bf8a6814
vinayv-456 <vinayvallabineni456@gmail.com>
2021-04-05 23:02:27 +0530

    Add changes to support singin and register route

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
-const Navigation = () => {
+const Navigation = ({route, handleRoute}) => {
   return (
-    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-      <p style={{textDecoration: 'underline', margin: '20px' }}>Sign Out</p>
-    </nav>
+    <div>
+    {
+      route === "home"
+      ?
+      <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <p style={{textDecoration: 'underline', margin: '20px' }}>Sign Out</p>
+      </nav>
+      :
+      <div>
+        {
+          route === "register"
+          ?
+          <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p style={{textDecoration: 'underline', margin: '20px' }}>signIn</p>
+            <div onClick={handleRoute("register")}>
+            <p style={{textDecoration: 'underline', margin: '20px', color:'yellow' }}>Regsiter</p>
+            </div>
+          </nav>
+          :
+          <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p style={{textDecoration: 'underline', margin: '20px', color:'yellow'}} >SignIn</p>
+            <p style={{textDecoration: 'underline', margin: '20px' }}>Regsiter</p>
+          </nav>
+        }
+      </div>
+    }
+    </div>
   );
 }
 
