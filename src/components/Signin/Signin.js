440e87d7a84bb5dc16a8e5e2145506507d52c96a
vinayv-456 <vinayvallabineni456@gmail.com>
2021-04-05 23:02:02 +0530

    Add Signin route

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axiosInstance from '../../apis/client'
 
 
-const Signin = () => {
+const Signin = ({handleRoute}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -17,14 +17,23 @@ const Signin = () => {
   const onSubmitSignIn = async() => {
     const result = await axiosInstance.post('/sign-in',{email: `${email}`, password: `${password}`})
     console.log(result);
+    if(result)
+    handleRoute("home");
   }
 
-    return (
-      <div>
-        <p>signIn</p>
+  return (
+    <div style={{display: 'flex', justifyContent:'center'}}>
+      <div style={{display:'flex', flexDirection:'column', alignContent:'center', width:'50%'}}>
+        Email:
+        <input type="text" onInput={onEmailChange} style={{marginBottom:'10px'}}/>
+        password:
+        <input type="text" onInput={onPasswordChange} style={{marginBottom: '20px'}}/>
+
+        <button onClick={onSubmitSignIn}>SignIn</button>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 
 export default Signin;
