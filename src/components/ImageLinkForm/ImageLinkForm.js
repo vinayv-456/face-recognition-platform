5db74de07fdb24f96deb1ffd8c989c0742ff1e54
vinayv-456 <vinay.vallabineni@emitrr.com>
2021-05-22 15:03:51 +0530

    display name and score of user in home screen

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+const ImageLinkForm = ({ name, onInputChange, onButtonSubmit, score }) => {
   return (
     <div>
       <p className='Discription center'>
-        {'This Magic Brain will detect faces in your pictures. Give a try.'}
+        {`Hello ${name}, This Magic Brain will detect faces in your pictures. Give a try.`}
       </p>
+      <h1 style={{textAlign: 'center', fontWeight:'normal'}}>score: {score}</h1>
       <div className='center'>
         <div className='form center'>
           <input type='text' className="imageUrl" onInput={onInputChange}/>
