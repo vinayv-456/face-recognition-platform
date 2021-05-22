c7c85eb7cd101f4c79a9bb8ad7db5bae714210f2
vinayv-456 <vinay.vallabineni@emitrr.com>
2021-05-22 19:07:27 +0530

    Add linking FR-api and display border

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './FaceRecognition.css';
+import axiosInstance from '../../apis/client'
+
 
 const FaceRecognition = ({ imageUrl }) => {
+  const [boundaryInfo, setBoundaryInfo] = useState({}) 
+  const [loading, setLoading] = useState(true)
+  useEffect(()=>{
+    if(imageUrl)
+    {
+      setLoading(true)
+      console.log("immmmage")
+      axiosInstance.get('/fr-image', {
+        params:{
+          imageUrl: imageUrl
+        }})
+      .then(res=>{
+        console.log("rws", res.data)
+        setBoundaryInfo(res.data)
+        setLoading(false)
+      })
+    }
+  }, [imageUrl])
+  
   return (
       <div className='bounding-box'>
-        <img id='inputimage' alt='' src={imageUrl} width='300px' heigh='300px'/>
+        {
+          imageUrl &&
+          <div style={{ position:'relative'}}>
+          <img id='input-image' alt='' src={imageUrl} width='300px' height='300px'/>
+          {
+            Object.keys(boundaryInfo).length!==0 && !loading &&
+            <div id="face-frame" style={{
+              left: (boundaryInfo?.left_col)*300+'px',
+              top: (boundaryInfo?.top_row)*300+'px',
+              width:(boundaryInfo?.right_col - boundaryInfo?.left_col)*300+'px',
+              height:(boundaryInfo?.bottom_row - boundaryInfo?.top_row)*300+'px'}}
+            />
+          }
+          </div>
+        }
+        
       </div>
   );
 }
